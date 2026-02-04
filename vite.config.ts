26221ae0993040c9d7494f2630ab819522f07332
lixuedenon <lixuedenon@gmail.com>
2026-02-03 23:52:01 -0800

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react', '@ffmpeg/ffmpeg', '@ffmpeg/util'],
+    exclude: ['lucide-react'],
   },
   server: {
     headers: {
